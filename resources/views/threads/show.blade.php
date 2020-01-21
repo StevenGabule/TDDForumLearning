f16110e80db6fe9c5394b6f007c84ec0d9ac385f
John Paul Lim Gabule <lucas.gabule@gmail.com>
2020-01-22 04:49:10 +0800

    a thread can have replies

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -10,7 +10,20 @@
                         {{ $thread->body }}
                     </div>
                 </div>
+            </div><!-- end of col-md-8 -->
+        </div><!-- end of row -->
+
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                @foreach($thread->replies as $reply)
+                    <div class="card mb-2 mt-2">
+                        <div class="card-header"><a href="#">{{$reply->owner->name}}</a> said {{ $reply->created_at->diffForHumans() }}</div>
+                        <div class="card-body">
+                            {{ $reply->body }}
+                        </div>
+                    </div>
+                @endforeach
             </div>
         </div>
-    </div>
+    </div><!-- end of container -->
 @endsection
